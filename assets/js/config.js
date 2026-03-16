de7f499449ccc0fddfd6083a1649730347460742
m0hamed8888 <150608097+m0hamed8888@users.noreply.github.com>
2026-03-16 22:16:05 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,4 +5,4 @@
 
 const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:5000/api'       // تطوير محلي
-  : 'https://YOUR_PROJECT.vercel.app/api';  // ← هتغير ده بعد الرفع على Vercel
+  : 'https://backend-mohsen-swimming-academy.vercel.app/api';  // ← هتغير ده بعد الرفع على Vercel
